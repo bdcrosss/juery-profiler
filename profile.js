b1be116eeaa714f136e467343cb151d6f86ab61f
Brice Cross <bdcross@purdue.edu>
2025-03-29 16:01:00 -0400

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,11 @@
+// the parentheses and format are v important for jquery implementation
+
+const profiler = ()=> {
+    $("#name").text("Brice Cross") ;
+    $("#major").text("Graphic Design") ;
+    $("#year").text("2026") ;
+    $("h4").text("Indiana") ;
+}
+
+$(document).ready(profiler)
+
